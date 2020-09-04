f49a047fb14c8ceda7cbb0248bf09afa73127708
Jean-Marie Favreau <J-Marie.Favreau@uca.fr>
2020-09-04 23:12:36 +0200

    remove a redundant "public"

--- application/app/src/main/java/com/jmtrivial/lepigeonnelson/broadcastplayer/messages/MessageCondition.java
+++ application/app/src/main/java/com/jmtrivial/lepigeonnelson/broadcastplayer/messages/MessageCondition.java
@@ -3,6 +3,6 @@ package com.jmtrivial.lepigeonnelson.broadcastplayer.messages;
 public interface MessageCondition {
 
 
-    public boolean satisfied(BMessage message);
+    boolean satisfied(BMessage message);
 
 }
